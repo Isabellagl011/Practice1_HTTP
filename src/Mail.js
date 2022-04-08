cbbf784f1b2edfbd49c8457959e7ebd5d38c904a
Isabella <isabella.grajalesl@autonoma.edu.co>
2022-04-08 18:58:23 -0500

    Se crearon las funciones para mensajes SMS , correo y WhatsApp.

---
+++ src/Mail.js
@@ -0,0 +1,49 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey('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')
+
+function sendEmailConfirmationHTML(customerName,orderNro){
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div class="container section">
+    <label>Paisaje</label>
+    <img src="https://www.privun.com/pub/media/catalog/product/cache/75eed2686e01eb22cb4050b2f40ddf97/h/t/httpsstorage.googleapis.comfotos-novaventaprivun2009389-lampara-mesa-serena-1.jpg"></img>
+
+  </div>
+</body>
+</html>`;
+}
+
+function getMessage(emailParams){
+  return{
+    to:emailParams.toEmail,
+    from: 'isabella.grajalesl@autonoma.edu.co',
+    subject:'Confirmación orden de compra Black Friday',
+    text:`Hola ${emailParams.customerName},Te enviamos las imágenes de los productos comprados , yla
+    factura con número ${emailParams.orderNro}. Gracias por tu compra`,
+    html:sendEmailConfirmationHTML(emailParams.customerName,emailParams.orderNro)
+
+  }
+}
+async function sendOrder(emailParams){
+  try{
+    await sgMail.send(getMessage(emailParams))
+    return {message:'confirmación de compra enviada'}
+
+  }catch(err){
+    const message='no se pudo enviar la orden de compra. Valide los errores'
+    console.error(message)
+    console.error(err)
+    if(err.response)console.error(err.response.body)
+    return{message}
+  }
+}
+module.exports={
+sendOrder
+}
